0a4e74d9947eed505e4e2e4aa35a121e310cce6a
Yuuki Rika <yuuki.rikachan@gmail.com>
2022-09-11 16:14:53 +0700

    Logging in to bot!

--- crates/examples/bot/src/main.rs
+++ crates/examples/bot/src/main.rs
@@ -1,3 +1,11 @@
-fn main() {
-    println!("Hello, world!");
+#[tokio::main]
+async fn main() {
+    let mut client = revolute::Client::new();
+
+    let mut token = String::new();
+
+    println!("Token: ");
+    std::io::stdin().read_line(&mut token).unwrap();
+
+    client.login_bot(token).await.unwrap();
 }
