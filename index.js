c5236336595e342498923ee0c0da5addef6a2b19
Yuuki Rika <yvvki@duck.com>
2023-07-21 01:56:35 +0700

    Use 2 space indent

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const openapiCargo = toml.parse(fs.readFileSync('crates/api/Cargo.toml'))
 const me = workspaceCargo['workspace']['package']['authors'][0]
 
 if (!openapiCargo['package']['authors'].includes(me)) {
-    openapiCargo['package']['authors'].push(me)
+  openapiCargo['package']['authors'].push(me)
 }
 
 // Inherit the workspace package table.
@@ -20,9 +20,9 @@ openapiCargo['package']['repository'] = { 'workspace': true };
 
 // Serialize
 fs.writeFileSync(
-    'crates/api/Cargo.toml',
-    toml.stringify(openapiCargo, {
-        newline: '\n',
-        newlineAround: 'section',
-    }).replaceAll('\'', '\"')
+  'crates/api/Cargo.toml',
+  toml.stringify(openapiCargo, {
+    newline: '\n',
+    newlineAround: 'section',
+  }).replaceAll('\'', '\"')
 )
