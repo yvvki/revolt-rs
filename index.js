e8cf4b840e7ea1c23347865685cbf4f0eecd5e9b
Yuuki Rika <yvvki@duck.com>
2023-07-21 02:28:44 +0700

    Refactor: variable name

--- index.js
+++ index.js
@@ -4,24 +4,24 @@ import toml from '@ltd/j-toml';
 
 // Deserialize
 const workspaceCargo = toml.parse(fs.readFileSync('Cargo.toml'));
-const openapiCargo = toml.parse(fs.readFileSync('crates/api/Cargo.toml'));
+const apiCargo = toml.parse(fs.readFileSync('crates/api/Cargo.toml'));
 
 // Me too :)
 const me = workspaceCargo['workspace']['package']['authors'][0];
 
-if (!openapiCargo['package']['authors'].includes(me)) {
-  openapiCargo['package']['authors'].push(me);
+if (!apiCargo['package']['authors'].includes(me)) {
+  apiCargo['package']['authors'].push(me);
 }
 
 // Inherit the workspace package table.
-openapiCargo['package']['license'] = { 'workspace': true };
-openapiCargo['package']['description'] = { 'workspace': true };
-openapiCargo['package']['repository'] = { 'workspace': true };
+apiCargo['package']['license'] = { 'workspace': true };
+apiCargo['package']['description'] = { 'workspace': true };
+apiCargo['package']['repository'] = { 'workspace': true };
 
 // Serialize
 fs.writeFileSync(
   'crates/api/Cargo.toml',
-  toml.stringify(openapiCargo, {
+  toml.stringify(apiCargo, {
     newline: '\n',
     newlineAround: 'section',
   }).replaceAll('\'', '\"')
